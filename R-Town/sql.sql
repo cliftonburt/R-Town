f7abe648b49833090a70903952335732464a6f72
Clifton Burt <88869618+ceebdata@users.noreply.github.com>
2021-08-20 18:05:46 -0700

    Update sql.sql

--- R-Town/sql.sql
+++ R-Town/sql.sql
@@ -1,6 +1,86 @@
+## SQL Structure 
+SELECT 
+## Column you want to look at
+FROM
+## Table the data lives in
+WHERE 
+## Certain condition on the data
+## WHERE is the filter
 GROUP BY
-
+## Column you want to aggregate by
+HAVING
+## Certain condition on the aggregation
 ORDER BY
+## Column you want to order results by and in ASCending or DESCending order
+LIMIT
+## The maximum number of rows you want your results to contain
 
-COUNT(*)
+=================
+
+JOINs
+
+
+
+=================
+##Aggregators
+SUM()
+	
+COUNT()
+	
+MAX()
+		
+MIN()
+	
+	
+	//Adding a DISTINCT clause inside the aggregator function
+	
+	//Using GROUP BY with aggregators
+==================	
+
+
+//When to use HAVING
+
+//Using ORDER BY to organize your results
+
+//When to use LIMIT
+
+
+
+//CASE Statements
+
+//IF statements
+
+//COALESCE() function
+	
+	
+	
+//Creating tables
+	
+//Deleting tables
+	
+//Dropping tables
+	
+//Temporary tables
+
+//Subroutines
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
