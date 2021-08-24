064d54dd9b310bcf3d3aa671ad62ea0e62b8d950
Clifton Burt <88869618+ceebdata@users.noreply.github.com>
2021-08-23 19:15:07 -0700

    Built-ins
    Python's built-in functions

---
+++ R-Town/py.py
@@ -0,0 +1,51 @@
+abs() #Takes in a numeric data type such as int or float and returns the absolute value of the argument
+
+all() # Returns True if every item in an iterable evaluates to True, otherwise, it returns False
+
+any() #Takes in an iterable object such as a list or tuple and returns True if any of the elements in the iterable are true. If none of the elements present in the iterable are true, any() will return False.
+
+ascii() # Receives as input an object containing string data, and returns the object as a printable representation with escapes for non-ASCII characters (accented characters)
+
+bin() #converts an integer into its binary equivalent string
+
+bool() #Converts a value to a Boolean True or False value
+
+breakpoint() # Engage, configure, and change the debugger program used in a script
+
+bytearray() #Returns an array of the given bytes of an object
+
+bytes() # Returns a byte immutable object representing the given bytes of an object
+
+callable() # Returns True if an object is callable, and False if an object is not callable
+
+chr() #Returns Unicode characters represented by integers ranging between 0 and 1,114,111
+
+classmethod() #Converts a given function into a class method
+
+compile() # Returns a runnable code object created from a string
+
+complex() # Converts a given string into a complex number
+
+delattr() # Allows the user to delete attributes from an object
+
+dict() # Initializes a new dictionary from mapping n-number of onject (key, value) pairs
+
+float() # Takes in a numeric, string, or no value and a copy of that value in the floating point data type
+
+input() # Takes in a value from the user an converts the value into a string
+
+int() # Take in a value that can be converted into an integer, a returns a copy of the value in the int datatype
+
+max() # Returns the highest value from values given or an iterable
+
+min() # Returns the lowest value from values given or iterable
+
+pow() # Returns the value of a base number x to the power of an exponent y, with an optional modulus z
+
+reversed() # Takes in an iterator object, such as a list or string, and returns a reversed iterator object
+
+round() # Takes a number and an integer as parameters, and returns the number with decimal places equal to the integer
+
+sorted() # Takes in an iterator object, such as a list, dictionary, set, or string, and sorts it according to a parameter
+
+zip() # Takes multiple iterators as input and returns a single zip object made up of a list of tuples 
